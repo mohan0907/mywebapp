e15c3abdaf4a447d9143da76f633aa11040ad299
mohan <64828834+mohan0907@users.noreply.github.com>
2021-04-06 15:57:40 +0530

    Create schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,18 @@
+
+ActiveRecord::Schema.define(version: 2021_04_02_111717) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
