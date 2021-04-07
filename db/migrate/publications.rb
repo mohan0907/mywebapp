69cddc13f5a14b959fefcb491a5b3ebde5ba4bbc
mohan <64828834+mohan0907@users.noreply.github.com>
2021-04-07 16:16:48 +0530

    Update publications.rb

--- db/migrate/publications.rb
+++ db/migrate/publications.rb
@@ -3,6 +3,10 @@ class CreatePublication <ActiveRecord :: Migration[6.0]
               create_table :publication do |t|
                     t.string :title 
                     t.boolean single_issue
+                    t.text :description
+                    t.references :publication_type
+                    t.integer :publisher_id
+                    t.string :publisher_type
               end
               add_index :publication, publication_type_id
          end
