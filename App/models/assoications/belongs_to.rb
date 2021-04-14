ceeb551a5312ecaec2b58c9f8d0f07e4f18d9503
mohan <64828834+mohan0907@users.noreply.github.com>
2021-04-14 13:13:37 +0530

    Update belongs_to.rb
    add foriegn key constraint

--- App/models/assoications/belongs_to.rb
+++ App/models/assoications/belongs_to.rb
@@ -14,7 +14,7 @@ class CreateBooks < ActiveRecord::migration[6]
       t.timestamps
     end
     create_table :books do |t|
-      t.belongs_to :author
+      t.belongs_to :author , foriegn_key :true # belongs_to does not ensure reference consistency so need to add a database-level foreign key constraint on the reference column
       t.datetime :published_at
       t.timestamps
     end  
